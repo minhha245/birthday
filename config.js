fcb9f4602ebc60a7db4bcd60717dae4041754099
halm <halm.paraline@gmail.com>
2024-04-01 12:31:15 +0700

    new

---
+++ config.js
@@ -0,0 +1,24 @@
+
+var config = {
+    // viết lời gửi gắm
+    texts: [
+        "Chúc Mừng Sinh Nhật",
+        "Idol_Thắng",
+        "Nhé~~",
+    ],
+
+    imgs: {
+        "Idol_Thắng": "./imgs/idol_thang.jpg",
+    },
+
+    desc: {
+        turn_on: "Bật Nên Nào",
+        play: "Music",
+        bannar_coming: "Thêm Màu Mè Đi",
+        balloons_flying: "Thiếu Gì Đó",
+        cake_fadein: "🎂 Đây !!!",
+        light_candle: "Đốt Nến Đi Pro",
+        wish_message: "WISH",
+        story: "Bao ae ăn bữa đi Idol",
+    }
+};
